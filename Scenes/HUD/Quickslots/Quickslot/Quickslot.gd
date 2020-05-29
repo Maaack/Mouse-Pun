812c90206b1b17adca4a8f5adbc45b0d10583329
Maaack <marek.belski@gmail.com>
2020-05-29 14:38:13 -0700

    MAP-01: Add visible selection box to quickslots.

--- Scenes/HUD/Quickslots/Quickslot/Quickslot.gd
+++ Scenes/HUD/Quickslots/Quickslot/Quickslot.gd
@@ -3,6 +3,7 @@ extends Control
 
 export(int,0,10) var slot : int setget set_slot
 export(Resource) var quantity setget set_quantity
+export(bool) var selected setget set_selected
 
 func set_slot(value:int):
 	$Label.text = str(value)
@@ -10,3 +11,7 @@ func set_slot(value:int):
 func set_quantity(value:AbstractQuantity):
 	quantity = value
 	$InventorySlot.quantity = quantity
+
+func set_selected(value:bool):
+	selected = value
+	$Selected.visible = selected
